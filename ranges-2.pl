5f059441216a0a2546517b1db93ea4a5bc9c0c2b
Mike Covington <mfcovington@gmail.com>
2013-05-26 15:09:16 -0700

    add subroutines to collapse ranges and get range length

--- ranges-2.pl
+++ ranges-2.pl
@@ -10,111 +10,70 @@ use warnings;
 use autodie;
 use feature 'say';
 use Data::Printer;
-use List::Util qw(first max);
+use List::Util qw(max);
 
 my %range;
 
-$range{10} = 1000000;
-$range{10} = 30;
-$range{20} = 60;
-$range{50} = 80;
-$range{100} = 800;
-$range{100} = 840;
-$range{100} = 850;
-$range{200} = 400;
-$range{1000} = 8000;
-$range{5000} = 9000;
+my $iterations = 5_000_000;
+
+for (1..$iterations) {
+    add_range( $_ , $_ + 5 , \%range );
+    # collapse_ranges(\%range) if $_ % 10000 == 0;
+    # collapse_ranges(\%range) if scalar keys %range > 1000;
+    # collapse_ranges(\%range);
+}
+
+collapse_ranges(\%range);
+
+p %range;
 
 sub add_range {
     my ( $start, $end, $range_ref ) = @_;
     if ( exists $range_ref->{$start} ) {
-        $range_ref->{$start} = max($end, $range_ref->{$start});
+        $range_ref->{$start} = max( $end, $range_ref->{$start} );
     }
     else {
         $range_ref->{$start} = $end;
     }
 }
 
-sub add_range_no_ref {
-    my ( $start, $end ) = @_;
-    if ( exists $range{$start} ) {
-        $range{$start} = max($end, $range{$start});
+sub collapse_ranges {
+    my $range_ref = shift;
+
+    my @cur_interval;
+    my @result;
+    my %temp_ranges;
+
+    for my $start ( sort { $a <=> $b } keys %$range_ref ) {
+        unless (@cur_interval) {
+            @cur_interval = ( $start, $range_ref->{$start} );
+            next;
+        }
+        my ( $cstart, $cend ) = @cur_interval;
+        if ( $start <= $cend + 1 ) {
+            @cur_interval = ( $cstart, max( $range_ref->{$start}, $cend ) );
+        }
+        else {
+            push @result, @cur_interval;
+            $temp_ranges{ $cur_interval[0] } = $cur_interval[1];
+            @cur_interval = ( $start, $range_ref->{$start} );
+        }
     }
-    else {
-        $range{$start} = $end;
-    }
+    push @result, @cur_interval;
+    $temp_ranges{ $cur_interval[0] } = $cur_interval[1];
+    %$range_ref = %temp_ranges;
 }
 
-my $iterations = 1_000_000;
-
-for (1..$iterations) {
-    add_range( $_ , $_ + 5 , \%range );
+sub range_length {
+    my $range_ref = shift;
+    my $length;
+    for ( keys %$range_ref) {
+        $length += $range_ref->{$_} - $_ + 1;
+    }
+    return $length;
 }
 
-# for (1..$iterations) {
-#     add_range_no_ref( $_ , $_ + 5 );
-# }
-
-# add_range( 100006, 100009 );
-
-my @cur_interval;
-my @result;
-my %final_ranges_1;
-
-for my $start ( sort { $a <=> $b } keys %range ) {
-    # say $start;
-    unless (@cur_interval) {
-        # say "sdfsdfsdf $start";
-        @cur_interval = ( $start, $range{$start});
-        next;
-    }
-    # say "@cur_interval";
-    my ( $cstart, $cend ) = @cur_interval;
-    if ( $start <= $cend + 1) {
-        @cur_interval = ( $cstart , max($range{$start}, $cend ));
-    }
-    else {
-        push @result, @cur_interval;
-        $final_ranges_1{$cur_interval[0]} = $cur_interval[1];
-        @cur_interval = ( $start, $range{$start});
-    }
-}
-push @result, @cur_interval;
-$final_ranges_1{$cur_interval[0]} = $cur_interval[1];
-
-my %final_ranges = @result;
-
-p %final_ranges;
-p %final_ranges_1;
-
-# p @cur_interval;
-
-p @result;
-
-# ranges = [
-#   (11, 15),
-#   (3, 9),
-#   (12, 14),
-#   (13, 20),
-#   (1, 5)]
-
-# result = []
-# cur = None
-# for start, stop in sorted(ranges): # sorts by start
-#   if cur is None:
-#     cur = (start, stop)
-#     continue
-#   cStart, cStop = cur
-#   if start <= cStop:
-#     cur = (cStart, max(stop, cStop))
-#   else:
-#     result.append(cur)
-#     cur = (start, stop)
-# result.append(cur)
-
-# print result
-
-
+say range_length( \%range );
 
 
 
