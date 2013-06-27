c6d828255cdc0b9e7c1edbd15954b2c134491a34
Mike Covington <mfcovington@gmail.com>
2013-06-27 14:37:07 -0700

    initial rm_ranges functionality (requires collapsed ranges)

--- lib/range.pm
+++ lib/range.pm
@@ -7,10 +7,12 @@ use strict;
 use warnings;
 use Exporter;
 use List::Util 'max';
+# use List::MoreUtils qw(lastidx lastval);
 use List::MoreUtils 'lastidx';
 
 our @ISA    = qw(Exporter);
-our @EXPORT = qw(add_range collapse_ranges range_length is_in_range);
+# our @EXPORT = qw(add_range collapse_ranges range_length is_in_range);
+our @EXPORT = qw(add_range collapse_ranges range_length is_in_range rm_range);
 
 sub add_range {
     my ( $start, $end, $range_ref ) = @_;
@@ -22,7 +24,32 @@ sub add_range {
     }
 }
 
-sub collapse_ranges {
+sub rm_range {    # should add to sub-hash otherwise collapse_ranges would need to be called first...
+    my ( $start, $end, $range_ref ) = @_;
+    my @starts = sort { $a <=> $b } keys %$range_ref;
+
+    my $left_start_idx  = lastidx { $_ < $start } @starts;
+    my $right_start_idx = lastidx { $_ <= $end } @starts;
+
+    my $left_start  = $starts[$left_start_idx];
+    my $right_start = $starts[$right_start_idx];
+
+    my $left_end  = $range_ref->{$left_start};
+    my $right_end = $range_ref->{$right_start};
+
+    if ( $right_start_idx - $left_start_idx > 0 ) {
+        delete @{$range_ref}
+          { @starts[ $left_start_idx + 1 .. $right_start_idx ] };
+    }
+    if ( $start < $left_end && $left_start_idx != -1 ) {
+        $range_ref->{$left_start} = $start - 1;
+    }
+    if ( $end >= $right_start && $end < $right_end ) {
+        $range_ref->{ $end + 1 } = $right_end;
+    }
+}
+
+sub collapse_ranges {    # collapse added ranges, then collapse to-be-removed ranges, then remove to-be-removed ranges from collapsed added ranges
     my $range_ref = shift;
 
     return unless %$range_ref;
