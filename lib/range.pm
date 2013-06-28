1581fbaa4b62cd9de237aa262b50213a46d7c174
Mike Covington <mfcovington@gmail.com>
2013-06-28 09:13:56 -0700

    complete rm_range functionality with updates to add_range and collapse_ranges

--- lib/range.pm
+++ lib/range.pm
@@ -9,47 +9,53 @@ use Exporter;
 use List::Util 'max';
 # use List::MoreUtils qw(lastidx lastval);
 use List::MoreUtils 'lastidx';
+use Carp;
 
 our @ISA    = qw(Exporter);
 # our @EXPORT = qw(add_range collapse_ranges range_length is_in_range);
 our @EXPORT = qw(add_range collapse_ranges range_length is_in_range rm_range);
 
 sub add_range {
-    my ( $start, $end, $range_ref ) = @_;
-    if ( exists $range_ref->{$start} ) {
-        $range_ref->{$start} = max( $end, $range_ref->{$start} );
+    _update_range( @_, 'add' );
+}
+
+sub rm_range {
+    _update_range( @_, 'rm' );
+}
+
+sub _update_range {
+    my ( $start, $end, $range_ref, $add_or_rm ) = @_;
+
+    if ( $start > $end ) {
+        carp "Warning: Range start ($start) is greater than range end ($end); values have been swapped";
+        ( $start, $end ) = ( $end, $start );
+    }
+
+    if ( exists $range_ref->{$add_or_rm}{$start} ) {
+        $range_ref->{$add_or_rm}{$start} = max( $end, $range_ref->{$add_or_rm}{$start} );
     }
     else {
-        $range_ref->{$start} = $end;
+        $range_ref->{$add_or_rm}{$start} = $end;
     }
+    $range_ref->{messy} = 1;
 }
 
-sub rm_range {    # should add to sub-hash otherwise collapse_ranges would need to be called first...
-    my ( $start, $end, $range_ref ) = @_;
-    my @starts = sort { $a <=> $b } keys %$range_ref;
+sub collapse_ranges {
+    my $range_ref = shift;
 
-    my $left_start_idx  = lastidx { $_ < $start } @starts;
-    my $right_start_idx = lastidx { $_ <= $end } @starts;
+    return if $range_ref->{messy} == 0;
 
-    my $left_start  = $starts[$left_start_idx];
-    my $right_start = $starts[$right_start_idx];
+    _collapse( $range_ref->{add} ) if $range_ref->{add};
 
-    my $left_end  = $range_ref->{$left_start};
-    my $right_end = $range_ref->{$right_start};
-
-    if ( $right_start_idx - $left_start_idx > 0 ) {
-        delete @{$range_ref}
-          { @starts[ $left_start_idx + 1 .. $right_start_idx ] };
+    if ( $range_ref->{rm} ) {
+        _collapse( $range_ref->{rm} );
+        _remove($range_ref);
     }
-    if ( $start < $left_end && $left_start_idx != -1 ) {
-        $range_ref->{$left_start} = $start - 1;
-    }
-    if ( $end >= $right_start && $end < $right_end ) {
-        $range_ref->{ $end + 1 } = $right_end;
-    }
+
+    $range_ref->{messy} = 0;
 }
 
-sub collapse_ranges {    # collapse added ranges, then collapse to-be-removed ranges, then remove to-be-removed ranges from collapsed added ranges
+sub _collapse {
     my $range_ref = shift;
 
     return unless %$range_ref;
@@ -76,6 +82,38 @@ sub collapse_ranges {    # collapse added ranges, then collapse to-be-removed ra
     }
     $temp_ranges{ $cur_interval[0] } = $cur_interval[1];
     %$range_ref = %temp_ranges;
+}
+
+sub _remove {
+    my $range_ref = shift;
+
+    my @starts = sort { $a <=> $b } keys %{$range_ref->{add}};
+
+    for my $start ( keys %{ $range_ref->{rm} } ) {
+        my $end = $range_ref->{rm}{$start};
+
+        my $left_start_idx  = lastidx { $_ < $start } @starts;
+        my $right_start_idx = lastidx { $_ <= $end } @starts;
+
+        my $left_start  = $starts[$left_start_idx];
+        my $right_start = $starts[$right_start_idx];
+
+        my $left_end  = $range_ref->{add}{$left_start};
+        my $right_end = $range_ref->{add}{$right_start};
+
+        if ( $right_start_idx - $left_start_idx > 0 ) {
+            delete @{ $range_ref->{add} }
+              { @starts[ $left_start_idx + 1 .. $right_start_idx ] };
+        }
+        if ( $start <= $left_end && $left_start_idx != -1 ) {
+            $range_ref->{add}{$left_start} = $start - 1;
+        }
+        if ( $end >= $right_start && $end < $right_end ) {
+            $range_ref->{add}{ $end + 1 } = $right_end;
+        }
+
+        delete ${ $range_ref->{rm} }{$start};
+    }
 }
 
 sub range_length {
